4bec715098a9955990830166c3452ccb5886d9fb
GokulPawar636 <gokulpawar636@gmail.com>
2026-01-18 11:49:17 +0530

    Security considerations in admin and privent from an hackers

--- Budget_Tracker/settings.py
+++ Budget_Tracker/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-later'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- Budget_Tracker/urls.py
+++ Budget_Tracker/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-budget/', admin.site.urls),
     path('', include('Tracker.urls')),
 ]
