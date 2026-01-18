d19a3631c0fefd4de217a18b4eafb6c522f66f01
GokulPawar636 <gokulpawar636@gmail.com>
2026-01-18 11:37:05 +0530

    Customising the admin interface

--- Tracker/admin.py
+++ Tracker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from Tracker.models import *    
 
-# Register your models here.
+
+admin.site.site_header = "Budget Tracker"
+admin.site.site_title = "Budget Tracker"    
+
+
+admin.site.register(CurrentBalance)
+
+
+class TrackingHistoryAdmin(admin.ModelAdmin):
+    list_display = ('description', 'amount', 'expense_type', 'created_at')
+    list_filter = ('expense_type',)
+    search_fields = ('description',)
+    ordering = ('-created_at',)
+
+    
+admin.site.register(TrackingHistory, TrackingHistoryAdmin)
--- Tracker/models.py
+++ Tracker/models.py
@@ -5,18 +5,24 @@ class CurrentBalance(models.Model):
     current_balance = models.FloatField(default = 0)
 
     def __str__(self) -> str:
-        return str(self.current_balance)
+        return f"The current balance is {self.current_balance}"
     
 
 
 class TrackingHistory(models.Model):
-    current_balance = models.ForeignKey(CurrentBalance , on_delete= models.CASCADE)
+    current_balance = models.ForeignKey(CurrentBalance, on_delete=models.CASCADE)
     amount = models.FloatField()
-    expense_type = models.CharField(choices =(('CREDIT' , 'CREDIT'), ('DEBIT', 'DEBIT')),max_length = 200)
-    description = models.CharField(max_length = 200)
-    created_at = models.DateTimeField(auto_now = True)
-    created_at = models.DateTimeField(auto_now_add = True)
 
+    expense_type = models.CharField(
+        max_length=10,
+        choices=[
+            ('CREDIT', 'CREDIT'),
+            ('DEBIT', 'DEBIT'),
+        ]
+    )
 
-    def __str__(self) -> str:
-        return f"The amount is {self.amount} for {self.description} expense type is {self.expense_type}"
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"The amount is {self.amount} for {self.description} on {self.created_at} with {self.expense_type}"
